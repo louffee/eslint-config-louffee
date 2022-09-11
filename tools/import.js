db8292a4287daf8db0b2bcd352e1697299f36eeb
Leonardo Lemos <leonardo.silva3@ab-inbev.com>
2022-09-10 22:11:44 -0300

    refactor: overwrite important style sets

--- tools/import.js
+++ tools/import.js
@@ -5,6 +5,35 @@ module.exports = {
   plugins: ['import'],
   rules: {
     'import/no-default-export': DISABLED,
+    'import/no-anonymous-default-export': ERROR,
+    'import/no-unresolved': ERROR,
+    'import/no-unassigned-imports': DISABLED,
+    'import/exports-last': ERROR,
+    'import/order': [
+      'error',
+      {
+        warnOnUnassignedImports: false,
+        'newlines-between': 'always',
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'builtin',
+            position: 'before',
+          },
+          {
+            pattern: 'react-*',
+            group: 'builtin',
+            position: 'before',
+          },
+          {
+            pattern: '@louffee/**',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling'],
+      },
+    ],
     'import/no-unassigned-import': [
       ERROR,
       {
